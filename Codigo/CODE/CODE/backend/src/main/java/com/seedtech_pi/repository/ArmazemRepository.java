467961f9e7e41e52283f55c0c373cfc4040d78c4
Vanda Vitorio <vandalarissa2710@gmail.com>
2025-12-10 12:02:11 -0300

    Delete Codigo/CODE/CODE directory

--- Codigo/CODE/CODE/backend/src/main/java/com/seedtech_pi/repository/ArmazemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.seedtech_pi.repository;
-
-import com.seedtech_pi.model.Armazem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArmazemRepository extends JpaRepository<Armazem, Integer> {}
